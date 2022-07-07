f3b481695352940fb5f5927a3f4ed6c588587f22
MrRedRhino <roman@deuerling.com>
2022-07-07 21:57:39 +0200

    Make ModifiableFileHelper returnable

--- src/main/java/org/pipeman/pipe_dl/util/ModifiableFileHelper.java
+++ src/main/java/org/pipeman/pipe_dl/util/ModifiableFileHelper.java
@@ -7,16 +7,17 @@ import java.nio.file.Path;
 import java.util.Map;
 
 public class ModifiableFileHelper {
-    public static void copyFile(Path path, Response response, Map<String, String> replacements) {
+    public static String copyFile(Path path, Response response, Map<String, String> replacements) {
         try {
             String content = Files.readString(path);
             for (Map.Entry<String, String> e : replacements.entrySet()) {
-                content = content.replaceAll(e.getKey(), e.getValue());
+                content = content.replace(e.getKey(), e.getValue());
             }
             response.raw().getOutputStream().write(content.getBytes());
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        return "";
     }
 }
