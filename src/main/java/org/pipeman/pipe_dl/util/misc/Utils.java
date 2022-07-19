1efb027bc402ab1eb0254f74b0b8e988ca134be8
MrRedRhino <roman@deuerling.com>
2022-07-19 18:46:17 +0200

    Add generateRandomString()

--- src/main/java/org/pipeman/pipe_dl/util/misc/Utils.java
+++ src/main/java/org/pipeman/pipe_dl/util/misc/Utils.java
@@ -1,5 +1,7 @@
 package org.pipeman.pipe_dl.util.misc;
 
+import java.util.Random;
+
 public class Utils {
     public static long[] parseToLongArray(String[] in) {
         long[] out = new long[in.length];
@@ -17,5 +19,17 @@ public class Utils {
         } catch (NumberFormatException ignored) {
             return null;
         }
+    }
+
+    public static String generateRandomString(int length) {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
